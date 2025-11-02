6ab0ed46e1056f496a2e9bc00f9c99c8a9c2211f
Hemang <hemangmishranew@gmail.com>
2025-11-02 21:43:15 +0530

    v1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.hexagraph.jagrati_android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.1.1"
+        versionCode = 6
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
